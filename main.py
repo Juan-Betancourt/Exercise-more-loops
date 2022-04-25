5bf330e215a54d20b395507115daecd88ad1abde
Juan Betancourt <juancbetancourt11@gmail.com>
2022-04-25 00:47:07 +0000

    Attempt of correct code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # 🚨 Don't change the code below 👇
-student_scores = input("Input a list of student scores ").split()
+student_scores = input("Input a list of student scores \n").split()
 for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 print(student_scores)
@@ -7,6 +7,10 @@ print(student_scores)
 
 #Write your code below this row 👇
 
+highest_score = 0
 
-
-
+for score in student_scores:
+  if highest_score <= student_scores:
+    highest_score()
+print(highest_score)
+  
